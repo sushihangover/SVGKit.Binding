34ccd9113b11d56fc6e4a36be749572a0bc7b3ed
SushiHangover <sushihangover@outlook.com>
2016-11-29 19:32:42 -0800

    Issue #1 Testing SVGKSourceString.SourceFromContentsOfString

--- Nuget.Test/Nuget.iOS.Text/ViewController.cs
+++ Nuget.Test/Nuget.iOS.Text/ViewController.cs
@@ -3,6 +3,7 @@ using Foundation;
 using UIKit;
 using SushiHangover.SVGKit;
 using System.IO;
+using System.Net.Http;
 
 namespace Nuget.iOS.Text
 {
@@ -21,6 +22,29 @@ namespace Nuget.iOS.Text
 			var imageView = new SVGKFastImageView(image);
 			imageView.Frame = View.Frame;
 			View.Add(imageView);
+
+			var uriString = "https://upload.wikimedia.org/wikipedia/commons/6/6b/Bitmap_VS_SVG.svg";
+			var button = new UIButton(UIButtonType.InfoDark);
+			button.Frame = new CoreGraphics.CGRect(100, 40, 100, 40);
+			button.SetTitle(uriString, UIControlState.Normal);
+			button.TouchUpInside += async (sender, e) =>
+			{
+				using (HttpClient client = new HttpClient())
+				using (HttpResponseMessage response = await client.GetAsync(uriString))
+				using (HttpContent content = response.Content)
+				{
+					string result = await content.ReadAsStringAsync();
+					if (result != null)
+					{
+						var svgSource = SVGKSourceString.SourceFromContentsOfString(result);
+						var svgImage = SVGKImage.ImageWithSource(svgSource);
+						imageView.Image = svgImage;
+						imageView.Frame = View.Frame;
+						imageView.ContentMode = UIViewContentMode.ScaleAspectFit;
+					}
+				}
+			};
+			View.Add(button);
 		}
 
 		public override void DidReceiveMemoryWarning()
